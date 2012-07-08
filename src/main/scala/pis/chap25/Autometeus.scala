3f0ef50e9c147ff56c56973a43d0ea0b0f396007
daclouds <daclouds@gmail.com>
2012-07-08 21:57:27 +0900

    영화 제목을 검색어로 시작하는 부분부터 검사하여 유사도 계산

--- src/main/scala/pis/chap25/Autometeus.scala
+++ src/main/scala/pis/chap25/Autometeus.scala
@@ -2,23 +2,37 @@ package pis.chap25
 
 import java.io.BufferedReader
 import java.io.FileReader
-
 import scala.collection.immutable.HashMap
-
 import com.codahale.jerkson.Json.stream
 
 object Autometeus extends App {
+  
+  def getTarget(input:String, title:String):String = {
+    var tokens = input.split(" ")
+    for (token <- tokens) {
+    	val idx = title.indexOf(token)
+    	if (idx != -1) return title.substring(title.indexOf(token))
+    }
+    return title
+  }
+  
   override def main(args: Array[String]) = {
 
     val reader = new BufferedReader(new FileReader("movies.json"))
-
-    //    Le jour avant le lendemain
-    var input: String = "Le      avant    lendemain"
+    if (args.length == 0) {
+      println("영화 제목을 입력해 주세요.")
+      exit()
+    }
+    var input: String = args.mkString(" ")
     var output = HashMap[String, Int]()
     
     var minDistance:Int = Int.MaxValue
     for (movie <- stream[Movie](reader)) {
-      var distance = StringDistance.stringDistance(input, movie.Title)
+      
+      var title = movie.Title.toLowerCase
+      var target = getTarget(input, title)
+      
+      var distance = StringDistance.stringDistance(input, target)
       if (distance < minDistance) {
         output += (movie.Title -> distance)
         output.toSeq.sortBy(_._2)
